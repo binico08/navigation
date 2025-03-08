b617c1c76c848553b17751b2084583f5238b7536
Nicole <envillegas@Anuga.local>
2025-03-08 08:31:58 +0800

    Removed login functionality and related components

--- App.js
+++ App.js
@@ -6,7 +6,6 @@ import { CartProvider } from './src/navigation/CartContext';
 import HomeScreen from './src/navigation/HomeScreen';
 import CartScreen from './src/navigation/CartScreen';
 import CheckoutScreen from './src/navigation/CheckoutScreen';
-import Login from './src/navigation/Login';
 
 const Stack = createStackNavigator();
 
@@ -20,7 +19,6 @@ export default function App() {
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Cart" component={CartScreen} />
           <Stack.Screen name="Checkout" component={CheckoutScreen} />
-          <Stack.Screen name="Login" component={Login} />
         </Stack.Navigator>
       </NavigationContainer>
     </CartProvider>
